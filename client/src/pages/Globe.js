dcd8df7d6a4c03a5ff3b1ce77717f6f388e6a542
Jared Seefried <jared.seefried@yahoo.com>
2020-12-04 10:49:34 -0700

    useEffect

--- client/src/pages/Globe.js
+++ client/src/pages/Globe.js
@@ -12,7 +12,6 @@ for(var i=0; i<covidCountries.length; i++){
   covidCountries[i].id = (i+1)
   covidCountries[i].color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6)
 }
-////////////////////////////////////////////////////////
 function markerTooltipRenderer(marker) {
   return `COUNTRY: ${marker.country} (Value: ${marker.value})`;
 }
@@ -58,74 +57,13 @@ function Globe() {
     }).catch(function (error) {
       console.error(error);
     });
+    setMarkers(randomMarkers)
   })
-  // ANDY ADD ENDS
-  ////////////////////////////////////////
-  function onClickMarker(marker, markerObject, event) {
-    setEvent({
-      type: "CLICK",
-      marker,
-      markerObjectID: markerObject.uuid,
-      pointerEventPosition: { x: event.clientX, y: event.clientY }
-    });
-    setDetails(markerTooltipRenderer(marker));
-  }
-  function onDefocus(previousFocus) {
-    setEvent({
-      type: "DEFOCUS",
-      previousFocus
-    });
-    setDetails(null);
-  }
   return (
-    <div>
-      {details && (
-        <div
-          style={{
-            background: "white",
-            position: "absolute",
-            fontSize: 20,
-            bottom: 0,
-            right: 0,
-            padding: 12
-          }}
-        >
-          <p>{details}</p>
-          <p>
-            EVENT: type={event.type}, position=
-            {JSON.stringify(event.pointerEventPosition)}
-          </p>
-        </div>
-      )}
-      <div style={{ padding: 32 }}>
-        <button onClick={() => setMarkers(randomMarkers)}>
-          Randomize markers
-        </button>
-        <button disabled={markers.length === 0} onClick={() => setMarkers([])}>
-          Clear markers
-        </button>
-        <button
-          disabled={markers.length === randomMarkers.length}
-          onClick={() =>
-            setMarkers([...markers, randomMarkers[markers.length]])
-          }
-        >
-          Add marker
-        </button>
-        <button
-          disabled={markers.length === 0}
-          onClick={() => setMarkers(markers.slice(0, markers.length - 1))}
-        >
-          Remove marker
-        </button>
-      </div>
+    <div className="globe">
       <ReactGlobe
-        height="100vh"
         markers={markers}
         options={options}
-        width="100vw"
-        onClickMarker={onClickMarker}
-        onDefocus={onDefocus}
       />
     </div>
   );
