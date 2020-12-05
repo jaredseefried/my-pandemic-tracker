d76db1eb86e5b6fc0524e8ff2d0508024e4ec04f
Jared Seefried <jared.seefried@yahoo.com>
2020-12-05 13:10:59 -0700

    fixed merging

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -3,28 +3,6 @@ import React, {useState, useEffect} from 'react'
 import './info.css'
 // import API from "../utils/covidAPI"
 
-
-function Info(props) {
-
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    var options = {
-      method: 'GET',
-      url: 'https://covid-19-tracking.p.rapidapi.com/v1',
-      headers: {
-        'x-rapidapi-key': 'aa4bbfbbc6msh943bc8aba837399p1827ebjsnde6bed3202fa',
-        'x-rapidapi-host': 'covid-19-tracking.p.rapidapi.com'
-      }
-    };
-    
-    axios.request(options).then(function (response) {
-      console.log(response.data);
-    }).catch(function (error) {
-      console.error(error);
-    });
-  })
-
 function Info(props) {
   return (
     <div className="info-container">
